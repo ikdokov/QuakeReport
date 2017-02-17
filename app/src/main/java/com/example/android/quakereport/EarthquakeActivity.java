658920cc6dbc009287e27c25f3caba3669c3bf61
ivandokov <ikdokov@gmail.com>
2017-02-17 18:09:36 +0200

    - Add settings screen

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,37 +17,58 @@ package com.example.android.quakereport;
 
 import android.app.Activity;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class EarthquakeActivity extends Activity implements LoaderManager.LoaderCallbacks<ArrayList<Earthquake>> {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
     private EarthquakeAdapter earthquakeAdapter;
     private static final String USGS_QUERY_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
 
     private static final int EARTHQUAKE_LOADER_ID = 1;
+    private TextView mEmptyView;
+    private TextView mNoInternetConnectionView;
+    private ListView mEarthquakeListView;
+
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.loading_spinner);
+
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        mEarthquakeListView = (ListView) findViewById(R.id.list);
+
+        mEmptyView = (TextView) findViewById(R.id.empty_view);
+        mNoInternetConnectionView = (TextView) findViewById(R.id.no_internet_connection);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        earthquakeAdapter = new EarthquakeAdapter(
-                this, new ArrayList<Earthquake>());
+        earthquakeAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mEarthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Earthquake currentEartquake = earthquakeAdapter.getItem(i);
@@ -60,7 +81,7 @@ public class EarthquakeActivity extends Activity implements LoaderManager.Loader
             }
         });
 
-        earthquakeListView.setAdapter(earthquakeAdapter);
+        mEarthquakeListView.setAdapter(earthquakeAdapter);
 
         getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
@@ -68,11 +89,45 @@ public class EarthquakeActivity extends Activity implements LoaderManager.Loader
 
     @Override
     public Loader<ArrayList<Earthquake>> onCreateLoader(int id, Bundle args) {
+        if (isConnected()) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+        }
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPreferences.getString(getString(R.string.settings_min_magnitude_key), getString(R.string.settings_min_magnitude_default));
+        String orderBy = sharedPreferences.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(USGS_QUERY_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
         return new EarthquakeLoader(this, USGS_QUERY_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<ArrayList<Earthquake>> loader, ArrayList<Earthquake> data) {
+        mProgressBar.setVisibility(View.GONE);
+
+        if (isConnected()) {
+            mEarthquakeListView.setEmptyView(mEmptyView);
+            mEmptyView.setText(R.string.no_earthquakes_found);
+            mEmptyView.setVisibility(View.VISIBLE);
+            mNoInternetConnectionView.setVisibility(View.GONE);
+        } else {
+            mEarthquakeListView.setEmptyView(null);
+            mEmptyView.setVisibility(View.GONE);
+            mNoInternetConnectionView.setVisibility(View.VISIBLE);
+        }
+
         earthquakeAdapter.clear();
 
         earthquakeAdapter.setEarthquakes(data);
@@ -84,4 +139,30 @@ public class EarthquakeActivity extends Activity implements LoaderManager.Loader
         earthquakeAdapter.clear();
     }
 
+    private boolean isConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
